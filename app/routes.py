e44f5d0c779fdcea3d5177acf7f0247fa02829cc
Matias Pardo <pardmatias@frba.utn.edu.ar>
2024-08-24 20:16:53 -0300

    actualizo request y response a alista de  url

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
-from flask import jsonify, request
-from app import app
-import base64
+from collections import defaultdict
+
 import cv2
 import numpy as np
+import requests
+from flask import jsonify, request
+
+from app import app
 from app import utils
+
 
 @app.route("/", methods=["GET"])
 def root():
@@ -13,28 +17,32 @@ def root():
 @app.route('/predicted', methods=['POST'])
 def upload_file():
     try:
-        if 'file' not in request.files:
-            return jsonify({"message": "No file part in the request", "status": "fail"}), 400
+        urls = request.json.get('urls')
 
-        file = request.files['file']
+        if not urls or not isinstance(urls, list):
+            return jsonify({"message": "No URLs provided or incorrect format", "status": "fail"}), 400
 
-        if file.filename == '':
-            return jsonify({"message": "No selected file", "status": "fail"}), 400
+        images_by_class = defaultdict(list)
 
-        file_data = file.read()
+        for url in urls:
+            response = requests.get(url)
+            if response.status_code != 200:
+                return jsonify({"message": f"Failed to fetch image from {url}", "status": "fail"}), 400
 
-        nparr = np.frombuffer(file_data, np.uint8)
-        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            file_data = response.content
+            nparr = np.frombuffer(file_data, np.uint8)
+            image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-        if image is None:
-            return jsonify({"error": "Failed to decode image. Ensure correct format."}), 500
+            if image is None:
+                return jsonify({"error": f"Failed to decode image from {url}. Ensure correct format."}), 500
 
-        predicted_class = utils.predict_class(image)
+            predicted_class = utils.predict_class(image)
+            images_by_class[predicted_class].append(url)
 
         return jsonify({
-            "message": "File successfully uploaded and predicted",
+            "message": "URLs successfully processed and predicted",
             "status": "success",
-            "predicted_class": predicted_class
+            "images_by_class": images_by_class
         })
 
     except Exception as e:
--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,4 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
